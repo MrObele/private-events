8682ebb4ba25a5ffe7b9d32d2c8be015edba3dc8
mrobele <obeledavid26@gmail.com>
2018-10-31 15:05:12 +0100

    Added events controller and action uncttionalities

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,35 @@
+class EventsController < ApplicationController
+  include SessionsHelper
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    
+  end
+
+  def create 
+    if logged_in?
+   @event = current_user.organized_events.build(event_params)
+    if @event.save
+      flash[:success] = "Event reated"
+      redirect_to current_user
+    else
+      render 'new'
+    end
+  else
+    redirect_to root_path
+  end
+  end
+
+  private 
+  
+  def event_params
+    params.require(:event).permit(:description,:location,:date)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/new'
+  get 'events/show'
   # get 'sessions/new'
   # get 'sessions/create'
   # get 'sessions/destroy'
  post 'sessions/new', to: 'sessions#create'
+ post 'events/new', to: 'events#create'
  root "users#new"
  resources :users
  resources :sessions, only: [:new]
+ resources :events
 end
