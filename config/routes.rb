3fd1984c41eb43ea8c32970bc0eff81e1c71a67d
mrobele <obeledavid26@gmail.com>
2018-11-01 12:05:06 +0100

    Added page layout with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
-Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/new'
-  get 'events/show'
-  # get 'sessions/new'
-  # get 'sessions/create'
-  # get 'sessions/destroy'
- post 'sessions/new', to: 'sessions#create'
- post 'events/new', to: 'events#create'
- root "users#new"
- resources :users
- resources :sessions, only: [:new]
- resources :events
-end
+   
+      Rails.application.routes.draw do
+        get 'events/index'
+        get 'events/new'
+        get 'events/show'
+        # get 'sessions/new'
+        # get 'sessions/create'
+        # get 'sessions/destroy'
+       get  '/login', to: 'sessions#new'
+       post  '/login', to: 'sessions#create'
+       post 'sessions/new', to: 'sessions#create'
+       post 'events/new', to: 'events#create'
+       root "events#index"
+       resources :users
+       resources :sessions, only: [:new]
+       resources :events
+      end
